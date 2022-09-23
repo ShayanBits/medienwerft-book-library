f4bfdb5d8ba177c6ac4cdbc418c1f4412513e2c7
shayan.shahpasand <shayan.shahpasand@meinestadt.de>
2022-09-23 10:59:04 +0200

    * imported fonts, normalize and milligram * implemented the Nav * added the three pages (Home, Detail and Favorites) * adjusted routes

--- src/App.vue
+++ src/App.vue
@@ -4,82 +4,136 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="column"><p class="logo">IT.Book.Library</p></div>
+        <div class="column">
+          <nav class="mainnav">
+            <RouterLink to="/" class="mainnav-link">Home</RouterLink>
+            <RouterLink to="/detail" class="mainnav-link ">Detail</RouterLink>
+            <RouterLink to="/favorites" class="mainnav-link">Favorites <span class="mainnav-number">2</span> </RouterLink>
+          </nav>
+        </div>
+      </div>
     </div>
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+
+<!--    </div>-->
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+
+nav a.router-link-exact-active {
+  color: var(--primary);
+  border-bottom-color: var(--primary);
+}
+
+/*nav a.router-link-exact-active:hover {*/
+/*  color: var(--primary);*/
+/*}*/
+
+/*nav a {*/
+/*  display: inline-block;*/
+/*  padding: 0 1rem;*/
+/*  border-left: 1px solid var(--color-border);*/
+/*}*/
+
+/*nav a:first-of-type {*/
+/*  border: 0;*/
+/*}*/
+
+/*@media (min-width: 1024px) {*/
+/*  header {*/
+/*    display: flex;*/
+/*    place-items: center;*/
+/*    padding-right: calc(var(--section-gap) / 2);*/
+/*  }*/
+
+/*  .logo {*/
+/*    margin: 0 2rem 0 0;*/
+/*  }*/
+
+/*  header .wrapper {*/
+/*    display: flex;*/
+/*    place-items: flex-start;*/
+/*    flex-wrap: wrap;*/
+/*  }*/
+
+/*  nav {*/
+/*    text-align: left;*/
+/*    margin-left: -1rem;*/
+/*    font-size: 1rem;*/
+
+/*    padding: 1rem 0;*/
+/*    margin-top: 1rem;*/
+/*  }*/
+/*}*/
+
+
+/* book-app styles*/
+
+
+
+.header {
+  background: #f4f5f6;
+  padding-top: 25px;
+  margin-bottom: 40px;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  color: var(--secondary);
+  font-weight: bold;
+  font-size: 2.7rem;
 }
 
-nav {
+.mainnav {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.mainnav-link {
+  margin: 0 10px;
+  line-height: 4.7rem;
+  color: var(--secondary);
+  border-bottom: 3px solid transparent;
+}
+
+.mainnav-link:hover {
+  color: var(--primary);
+}
+
+.mainnav-link.mainnav-link--active {
+  color: var(--primary);
+  border-bottom-color: var(--primary);
+}
+
+.mainnav-number {
+  background-color: var(--primary);
+  color: #f4f5f6;
+  padding: 0 5px;
+  border-radius: 50%;
+}
+
+main .fav-btn {
+  padding: 0;
+  margin-bottom: -45px;
+}
+
+.fav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100%;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.first-col {
+  width: 5%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1> Favorites </h1>
+  </main>
+</template>
